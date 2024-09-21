166a870cf063a5ad5f6987367ca8f444ab019c1a
Joao Vitor Marques <jvsilvam@outlook.com>
2024-09-20 22:22:28 -0300

    refactor: verify if receiver already exists

--- src/main/java/com/informatica/controle_material/data/usecase/receiver/AddReceiverImpl.java
+++ src/main/java/com/informatica/controle_material/data/usecase/receiver/AddReceiverImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.informatica.controle_material.data.dto.receiver.AddReceiverDTO;
+import com.informatica.controle_material.data.exception.AlreadyExistsException;
 import com.informatica.controle_material.domain.model.Receiver;
 import com.informatica.controle_material.domain.usecases.receiver.AddReceiverUseCase;
 import com.informatica.controle_material.infra.repository.ReceiverRepository;
@@ -18,6 +19,10 @@ public class AddReceiverImpl implements AddReceiverUseCase {
   @Transactional
   @Override
   public Receiver execute(AddReceiverDTO addReceiverDTO) {
+    if (receiverRepository.findByCpf(addReceiverDTO.cpf()).isPresent()) {
+      throw new AlreadyExistsException("O CPF informado já está cadastrado");
+    }
+  
     Receiver receiver = addReceiverDTO.toModel();
     
     return receiverRepository.save(receiver);
--- src/main/java/com/informatica/controle_material/infra/repository/ReceiverRepository.java
+++ src/main/java/com/informatica/controle_material/infra/repository/ReceiverRepository.java
@@ -1,9 +1,13 @@
 package com.informatica.controle_material.infra.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.informatica.controle_material.domain.model.Receiver;
 
 public interface ReceiverRepository extends JpaRepository<Receiver, Long> {
   
+  Optional<Receiver> findByCpf(String cpf);
+
 }
